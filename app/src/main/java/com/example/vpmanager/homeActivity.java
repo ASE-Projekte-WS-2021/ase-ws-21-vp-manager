d61b6badd78cf3b3521f4db324abb3376a15c6b7
KilianBogner <kilian-bogner@t-online.de>
2022-02-10 01:54:13 +0100

    Deleted an unused import

--- app/src/main/java/com/example/vpmanager/homeActivity.java
+++ app/src/main/java/com/example/vpmanager/homeActivity.java
@@ -8,7 +8,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -82,7 +81,7 @@ public class homeActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-                switch(item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.nav_home:
                         break;
                     case R.id.nav_search:
