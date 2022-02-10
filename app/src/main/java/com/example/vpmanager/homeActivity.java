41367c6a7d2a39599f938af0e81e5e8ac3f0cd10
KilianBogner <kilian-bogner@t-online.de>
2022-02-10 18:45:53 +0100

    Deleted unused import statement

--- app/src/main/java/com/example/vpmanager/homeActivity.java
+++ app/src/main/java/com/example/vpmanager/homeActivity.java
@@ -10,7 +10,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
