870454b2a5a40ae98d7803ca4b4504220db4b46e
Mutigo <kai.aslan1996@gmail.com>
2022-02-28 23:25:38 +0100

    Added a progress bar
    Because:
    - better overview
    
    Better overview for the user so he can always see where he is currently at in the creation process and will therefore ease
    the navigation within the study creation.
    
    https://github.com/kofigyan/StateProgressBar

--- app/src/main/java/com/example/vpmanager/createStudyBase.java
+++ app/src/main/java/com/example/vpmanager/createStudyBase.java
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.kofigyan.stateprogressbar.StateProgressBar;
 
 import java.util.Objects;
 
@@ -23,7 +24,10 @@ public class createStudyBase extends AppCompatActivity {
     String room = "";
     String contact = "";
 
-    int currentFragment = 1;
+    StateProgressBar stateProgressBar;
+    String[] descriptionData = {"Basis", "Beschreibung", "Art", "Termine", "Bestätigen"};
+
+    int currentFragment = 0;
 
     Button back;
     Button next;
@@ -49,10 +53,10 @@ public class createStudyBase extends AppCompatActivity {
     private void setupView() {
         fragmentManager = getSupportFragmentManager();
         fragment_container = fragmentManager.findFragmentById(R.id.fragment_container);
-        textInputEditTextTitle = fragmentManager.getFragments().get(0).getView().findViewById(R.id.inputFieldTitle);
-        textInputEditTextVP = fragmentManager.getFragments().get(0).getView().findViewById(R.id.inputFieldVP);
         back = findViewById(R.id.backButton);
         next = findViewById(R.id.nextButton);
+        stateProgressBar = (StateProgressBar) findViewById(R.id.your_state_progress_bar_id);
+        stateProgressBar.setStateDescriptionData(descriptionData);
     }
 
 
@@ -60,67 +64,145 @@ public class createStudyBase extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                switch (currentFragment){
-                    case 1:
-                        getInput(currentFragment);
-                        fragmentManager
-                                .beginTransaction()
-                                .setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left,
-                                        R.anim.enter_from_left, R.anim.exit_to_right)
-                                .replace(R.id.fragment_container, new createStudyFragment_StepOne(), null)
-                                .addToBackStack(null)
-                                .commit();
-                        System.out.println("Frag 1 , Actually: " + currentFragment);
-                        currentFragment++;
-                        break;
-                    case 2:
-                        getInput(currentFragment-1);
-                        fragmentManager
-                                .beginTransaction()
-                                .setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left,
-                                        R.anim.enter_from_left, R.anim.exit_to_right)
-                                .replace(R.id.fragment_container, new createStudyFragment_StepTwo(), null)
-                                .addToBackStack(null)
-                                .commit();
-                        System.out.println("Frag 2 , Actually: " + currentFragment);
-                        currentFragment++;
-                        break;
-                    case 3:
-                        getInput(currentFragment-1);
-                        fragmentManager
-                                .beginTransaction()
-                                .setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left,
-                                        R.anim.enter_from_left, R.anim.exit_to_right)
-                                .replace(R.id.fragment_container, new createStudyFragment_StepThree(), null)
-                                .addToBackStack(null)
-                                .commit();
-                        System.out.println("Frag 3 , Actually: " + currentFragment);
-                        currentFragment++;
-                        break;
-                    default:
-                        break;
-                }
+                nextButton();
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backButton();
             }
         });
     }
 
-    private void pageLoad(){
-
+    private void nextButton() {
+        switch (currentFragment) {
+            case 0:
+                stateProgressBar.setCurrentStateNumber(StateProgressBar.StateNumber.ONE);
+                fragmentManager
+                        .beginTransaction()
+                        .setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left,
+                                R.anim.enter_from_left, R.anim.exit_to_right)
+                        .replace(R.id.fragment_container, new createStudyFragment_StepOne(), null)
+                        .addToBackStack(null)
+                        .commit();
+                System.out.println("Before Currentfragment = " + currentFragment);
+                currentFragment++;
+                System.out.println("Currentfragment = " + currentFragment);
+                break;
+            case 1:
+                getInput(currentFragment);
+                stateProgressBar.setCurrentStateNumber(StateProgressBar.StateNumber.ONE);
+                fragmentManager
+                        .beginTransaction()
+                        .setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left,
+                                R.anim.enter_from_left, R.anim.exit_to_right)
+                        .replace(R.id.fragment_container, new createStudyFragment_StepTwo(), null)
+                        .addToBackStack(null)
+                        .commit();
+                System.out.println("Before Currentfragment = " + currentFragment);
+                currentFragment++;
+                System.out.println("Currentfragment = " + currentFragment);
+                break;
+            case 2:
+                getInput(currentFragment);
+                stateProgressBar.setCurrentStateNumber(StateProgressBar.StateNumber.TWO);
+                fragmentManager
+                        .beginTransaction()
+                        .setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left,
+                                R.anim.enter_from_left, R.anim.exit_to_right)
+                        .replace(R.id.fragment_container, new createStudyFragment_StepThree(), null)
+                        .addToBackStack(null)
+                        .commit();
+                System.out.println("Before Currentfragment = " + currentFragment);
+                currentFragment++;
+                System.out.println("Currentfragment = " + currentFragment);
+                break;
+            case 3:
+                getInput(currentFragment);
+                stateProgressBar.setCurrentStateNumber(StateProgressBar.StateNumber.THREE);
+                fragmentManager
+                        .beginTransaction()
+                        .setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left,
+                                R.anim.enter_from_left, R.anim.exit_to_right)
+                        .replace(R.id.fragment_container, new createStudyFragment_StepThree(), null)
+                        .addToBackStack(null)
+                        .commit();
+                System.out.println("Before Currentfragment = " + currentFragment);
+                currentFragment++;
+                System.out.println("Currentfragment = " + currentFragment);
+                break;
+            default:
+                break;
+        }
     }
 
-    private void getInput(int page){
-        if(page == 1) {
+    private void backButton() {
+        switch (currentFragment) {
+            case 1:
+                getInput(currentFragment);
+                stateProgressBar.setCurrentStateNumber(StateProgressBar.StateNumber.ONE);
+                fragmentManager
+                        .beginTransaction()
+                        .setCustomAnimations(R.anim.enter_from_left, R.anim.exit_to_right,
+                                R.anim.enter_from_right, R.anim.exit_to_left)
+                        .replace(R.id.fragment_container, new createStudyFragment_Base(), null)
+                        .addToBackStack(null)
+                        .commit();
+                System.out.println("Before Currentfragment = " + currentFragment);
+                currentFragment--;
+                System.out.println("Currentfragment = " + currentFragment);
+                break;
+            case 2:
+                getInput(currentFragment);
+                stateProgressBar.setCurrentStateNumber(StateProgressBar.StateNumber.ONE);
+                fragmentManager
+                        .beginTransaction()
+                        .setCustomAnimations(R.anim.enter_from_left, R.anim.exit_to_right,
+                                R.anim.enter_from_right, R.anim.exit_to_left)
+                        .replace(R.id.fragment_container, new createStudyFragment_StepOne(), null)
+                        .addToBackStack(null)
+                        .commit();
+                System.out.println("Before Currentfragment = " + currentFragment);
+                currentFragment--;
+                System.out.println("Currentfragment = " + currentFragment);
+                break;
+            case 3:
+                getInput(currentFragment);
+                stateProgressBar.setCurrentStateNumber(StateProgressBar.StateNumber.TWO);
+                fragmentManager
+                        .beginTransaction()
+                        .setCustomAnimations(R.anim.enter_from_left, R.anim.exit_to_right,
+                                R.anim.enter_from_right, R.anim.exit_to_left)
+                        .replace(R.id.fragment_container, new createStudyFragment_StepTwo(), null)
+                        .addToBackStack(null)
+                        .commit();
+                System.out.println("Before Currentfragment = " + currentFragment);
+                currentFragment--;
+                System.out.println("Currentfragment = " + currentFragment);
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    private void getInput(int page) {
+        if (page == 1) {
+            textInputEditTextTitle = fragmentManager.getFragments().get(0).getView().findViewById(R.id.inputFieldTitle);
+            textInputEditTextVP = fragmentManager.getFragments().get(0).getView().findViewById(R.id.inputFieldVP);
             studyTitle = Objects.requireNonNull(textInputEditTextTitle.getText()).toString();
             VP = Objects.requireNonNull(textInputEditTextVP.getText()).toString();
+            System.out.println(VP + " " + studyTitle);
         }
-        System.out.println(page == 2);
-        if(page == 2){
+        if (page == 2) {
             textInputEditTextDesc = fragmentManager.getFragments().get(0).getView().findViewById(R.id.inputFieldStudyDesc);
             studyDesc = Objects.requireNonNull(textInputEditTextDesc.getText()).toString();
+            System.out.println(studyDesc);
         }
 
-        if(page == 3){
-
+        if (page == 3) {
+            System.out.println("Poggers");
         }
     }
 
