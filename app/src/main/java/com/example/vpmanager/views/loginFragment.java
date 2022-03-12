6d88d53d4ce36b91be6927bbbac8ecbd0a307296
Mutigo <kai.aslan1996@gmail.com>
2022-03-12 19:11:56 +0100

    Addition of Login Xml
    Because:
    - providing a layout for function
    
    First draft of the XMl should provide the user a intutive login and
    registration process

---
+++ app/src/main/java/com/example/vpmanager/views/loginFragment.java
@@ -0,0 +1,43 @@
+package com.example.vpmanager.views;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.vpmanager.R;
+
+
+public class loginFragment extends Fragment {
+
+
+    public loginFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        setupView(view);
+    }
+
+    private void setupView(View view){
+
+    }
+}
