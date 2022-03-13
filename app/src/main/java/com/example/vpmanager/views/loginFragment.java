bb653c270f60e8f013ce8195daabb9fbdb9bd01e
Mutigo <kai.aslan1996@gmail.com>
2022-03-13 22:40:06 +0100

    Removed unused import
    Because:
    - not checkstyle conform

--- app/src/main/java/com/example/vpmanager/views/loginFragment.java
+++ app/src/main/java/com/example/vpmanager/views/loginFragment.java
@@ -21,7 +21,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 
 public class loginFragment extends Fragment {
