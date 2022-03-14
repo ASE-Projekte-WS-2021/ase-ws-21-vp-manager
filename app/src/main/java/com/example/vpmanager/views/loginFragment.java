022767e909e18b9bfe80bda9eb64e7341e421a3e
Mutigo <kai.aslan1996@gmail.com>
2022-03-14 22:21:05 +0100

    Removed unused Imports
    Because:
    - improve code readability

--- app/src/main/java/com/example/vpmanager/views/loginFragment.java
+++ app/src/main/java/com/example/vpmanager/views/loginFragment.java
@@ -1,7 +1,5 @@
 package com.example.vpmanager.views;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,7 +12,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.vpmanager.R;
