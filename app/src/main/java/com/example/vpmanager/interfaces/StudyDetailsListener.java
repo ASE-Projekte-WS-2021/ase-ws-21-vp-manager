4a1d5f4ed4004711285130dd72bacd654572e86b
KilianBogner <kilian-bogner@t-online.de>
2022-03-18 18:05:22 +0100

    Added the viewModel and repository classes
    Because:
    - These are needed for the mvvm pattern
    
    Also added interfaces, for calling methods when the database calls are finished

---
+++ app/src/main/java/com/example/vpmanager/interfaces/StudyDetailsListener.java
@@ -0,0 +1,7 @@
+package com.example.vpmanager.interfaces;
+
+import com.example.vpmanager.models.StudyDetailModel;
+
+public interface StudyDetailsListener {
+    void onStudyDetailsReady(StudyDetailModel studyDetailModel);
+}
---
+++ app/src/main/java/com/example/vpmanager/viewmodels/StudyViewModel.java
@@ -0,0 +1,99 @@
+package com.example.vpmanager.viewmodels;
+
+import androidx.lifecycle.ViewModel;
+
+import com.example.vpmanager.interfaces.SelectUnselectDateListener;
+import com.example.vpmanager.interfaces.StudyDatesListener;
+import com.example.vpmanager.interfaces.StudyDetailsListener;
+import com.example.vpmanager.models.DateModel;
+import com.example.vpmanager.models.StudyDetailModel;
+import com.example.vpmanager.repositories.StudyRepository;
+import com.example.vpmanager.views.StudyDates;
+import com.example.vpmanager.views.StudyDetails;
+
+import java.util.ArrayList;
+
+public class StudyViewModel extends ViewModel implements StudyDetailsListener, StudyDatesListener, SelectUnselectDateListener {
+
+    public StudyDetails studyDetails;
+    public StudyDates studyDates;
+
+    private StudyDetailModel mStudyDetails;
+    private ArrayList<DateModel> mStudyDates;
+
+    private ArrayList<String> userIdsOfDates = new ArrayList<>();
+    private DateModel selectedDateObject;
+
+    private StudyRepository mStudyRepo;
+
+    public void prepareRepo() {
+        mStudyRepo = StudyRepository.getInstance();
+        //Instance is the same but different data can be retrieved!
+        mStudyRepo.setFirestoreCallback(this, this, this);
+    }
+
+    public void fetchStudyDetails(String currentStudyId) {
+        mStudyRepo.getStudyDetails(currentStudyId);
+    }
+
+    public void fetchStudyDates(String currentStudyId, String currentUserId) {
+        mStudyRepo.getStudyDates(currentStudyId, currentUserId);
+    }
+
+    public void selectDate(String dateId, String currentUserId) {
+        mStudyRepo.selectDate(dateId, currentUserId);
+    }
+
+    public void unselectDate() {
+        String selectedDateId = selectedDateObject.getDateId();
+        mStudyRepo.unselectDate(selectedDateId);
+    }
+
+    public StudyDetailModel getStudyDetails() {
+        return mStudyDetails;
+    }
+
+    public ArrayList<DateModel> getStudyDates() {
+        return mStudyDates;
+    }
+
+    public ArrayList<String> getUserIdsOfDates() {
+        return userIdsOfDates;
+    }
+
+    public DateModel getSelectedDateObject() {
+        return selectedDateObject;
+    }
+
+    public void findSelectedDateObject(String currentUserId) {
+        if (userIdsOfDates.contains(currentUserId)) {
+            //get the dateModelObject at the same position and safe it in a variable
+            int position = userIdsOfDates.indexOf(currentUserId);
+            selectedDateObject = new DateModel();
+            selectedDateObject = mStudyDates.get(position);
+        }
+    }
+
+    @Override
+    public void onStudyDetailsReady(StudyDetailModel studyDetailModel) {
+        mStudyDetails = studyDetailModel;
+        studyDetails.setStudyDetails();
+    }
+
+    @Override
+    public void onStudyDatesReady(ArrayList<DateModel> datesArrayList) {
+        mStudyDates = datesArrayList;
+
+        //set the list of userIds
+        userIdsOfDates.clear();
+        for (int i = 0; i < mStudyDates.size(); i++) {
+            userIdsOfDates.add(mStudyDates.get(i).getUserId());
+        }
+        studyDates.setDatesView();
+    }
+
+    @Override
+    public void onDateActionFinished() {
+        studyDates.reloadDates();
+    }
+}
