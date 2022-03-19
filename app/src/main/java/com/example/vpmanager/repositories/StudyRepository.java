b1190f858c99eb0acde1d2a5e3a13f2c6fcb9fb6
KilianBogner <kilian-bogner@t-online.de>
2022-03-19 17:29:57 +0100

    Deleted an unused import statement

--- app/src/main/java/com/example/vpmanager/repositories/StudyRepository.java
+++ app/src/main/java/com/example/vpmanager/repositories/StudyRepository.java
@@ -5,7 +5,6 @@ import static android.content.ContentValues.TAG;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import com.example.vpmanager.interfaces.SelectDateListener;
 import com.example.vpmanager.interfaces.UnselectDateListener;
