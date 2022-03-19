08c68f0b16fb66ca37d889590f71964459c843cc
Mutigo <kai.aslan1996@gmail.com>
2022-03-19 16:54:05 +0100

    Set mail input to inactive
    Because:
    - user should know its not changeable
    
    Email input in study creation is now gray, so the user knows that its
     final.

--- app/src/main/java/com/example/vpmanager/createStudy/createStudyFragment_StepTwo.java
+++ app/src/main/java/com/example/vpmanager/createStudy/createStudyFragment_StepTwo.java
@@ -2,6 +2,7 @@ package com.example.vpmanager.createStudy;
 
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -48,6 +49,7 @@ public class createStudyFragment_StepTwo extends Fragment {
     private void setupView(View view){
         textInputEditTextContactMail = view.findViewById(R.id.inputContact1);
         textInputEditTextContactMail.setFocusable(false);
+        textInputEditTextContactMail.setTextColor(ContextCompat.getColor(getActivity(), R.color.pieChartRemaining));
         textInputEditTextContactPhone = view.findViewById(R.id.inputContact2);
         textInputEditTextContactSkype = view.findViewById(R.id.inputContact3);
         textInputEditTextContactDiscord = view.findViewById(R.id.inputContact4);
