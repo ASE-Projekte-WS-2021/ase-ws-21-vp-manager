636cd16dad669148a21c547bc23bbe1242b54aed
Niklas_Jahning <tokimetakaari.zukarito@googlemail.com>
2022-03-20 21:29:51 +0100

    unit test fix

--- app/src/main/java/com/example/vpmanager/repositories/StudyRepository.java
+++ app/src/main/java/com/example/vpmanager/repositories/StudyRepository.java
@@ -17,6 +17,8 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
