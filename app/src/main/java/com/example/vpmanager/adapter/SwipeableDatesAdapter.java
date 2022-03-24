ec052d7b838990a616451a1f158c9a3a5cd49c5d
KilianBogner <kilian-bogner@t-online.de>
2022-03-24 16:59:47 +0100

    The list of dates in the create study process is now swipeable
    Because:
    - This improves the quality of the user experience
    
    With a (left or right) swipe any item can be deleted. A snackBar notifies the user about the action and provides an opportunity to undo the deletion.

--- app/src/main/java/com/example/vpmanager/adapter/SwipeableDatesAdapter.java
+++ app/src/main/java/com/example/vpmanager/adapter/SwipeableDatesAdapter.java
@@ -12,17 +12,25 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.vpmanager.R;
 import com.example.vpmanager.models.DateModel;
+import com.example.vpmanager.views.createStudy.createStudyFragment_StepFive;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
 public class SwipeableDatesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    private Context mContext;
+    public Context mContext;
     private ArrayList<String> preselectedDates; //DateModel //only stringList not modelList!
 
-    public SwipeableDatesAdapter(Context context, ArrayList<String> studyDates) {
+    //new with swipeToDelete
+    private String mRecentlyDeletedItem;
+    private int mRecentlyDeletedItemPosition;
+    private View mFragmentView;
+
+    public SwipeableDatesAdapter(Context context, ArrayList<String> studyDates, View fragmentView) {
         mContext = context;
         preselectedDates = studyDates;
+        mFragmentView = fragmentView;
         Log.d("SwipeableDatesAdapter", "constructor was called");
     }
 
@@ -57,6 +65,30 @@ public class SwipeableDatesAdapter extends RecyclerView.Adapter<RecyclerView.Vie
             dateDate = itemView.findViewById(R.id.dateItemDate);
             dateOtherInfos = itemView.findViewById(R.id.dateItemLayoutProposal);
         }
+    }
+
+    //new with swipeToDelete
+    public void deleteItem(int position){
+        mRecentlyDeletedItem = preselectedDates.get(position);
+        mRecentlyDeletedItemPosition = position;
+        preselectedDates.remove(position);
+        notifyItemRemoved(position);
+        showUndoSnackBar();
+    }
+
+    //new with swipeToDelete
+    private void showUndoSnackBar(){
+        View view = mFragmentView.findViewById(R.id.create_step_five_layout);
+        Snackbar snackbar = Snackbar.make(view, "1 Termin gelöscht",
+                Snackbar.LENGTH_LONG);
+        snackbar.setAction("Rückgängig", v -> undoDelete());
+        snackbar.show();
+    }
+
+    //new with swipeToDelete
+    private void undoDelete(){
+        preselectedDates.add(mRecentlyDeletedItemPosition, mRecentlyDeletedItem);
+        notifyItemInserted(mRecentlyDeletedItemPosition);
     }
 
 }
---
+++ app/src/main/java/com/example/vpmanager/helper/SwipeToDeleteCallback.java
@@ -0,0 +1,120 @@
+package com.example.vpmanager.helper;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.vpmanager.R;
+import com.example.vpmanager.adapter.SwipeableDatesAdapter;
+
+public class SwipeToDeleteCallback extends ItemTouchHelper.SimpleCallback {
+    /*
+     * Creates a Callback for the given drag and swipe allowance. These values serve as
+     * defaults
+     * and if you want to customize behavior per ViewHolder, you can override
+     * {@link #getSwipeDirs(RecyclerView, ViewHolder)}
+     * and / or {@link #getDragDirs(RecyclerView, ViewHolder)}.
+     *
+     * @param dragDirs  Binary OR of direction flags in which the Views can be dragged. Must be
+     *                  composed of {@link #LEFT}, {@link #RIGHT}, {@link #START}, {@link
+     *                  #END},
+     *                  {@link #UP} and {@link #DOWN}.
+     * @param swipeDirs Binary OR of direction flags in which the Views can be swiped. Must be
+     *                  composed of {@link #LEFT}, {@link #RIGHT}, {@link #START}, {@link
+     *                  #END},
+     *                  {@link #UP} and {@link #DOWN}.
+     */
+
+    private SwipeableDatesAdapter mAdapter;
+    private Drawable icon;
+    private final ColorDrawable background;
+
+    public SwipeToDeleteCallback(SwipeableDatesAdapter adapter) {
+        super(0, ItemTouchHelper.RIGHT | ItemTouchHelper.LEFT);
+        mAdapter = adapter;
+        icon = ContextCompat.getDrawable(mAdapter.mContext, R.drawable.ic_baseline_delete_24);
+        background = new ColorDrawable(Color.RED);
+    }
+
+    @Override
+    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+        return false;
+    }
+
+    @Override
+    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+        int position = viewHolder.getAdapterPosition();
+        mAdapter.deleteItem(position);
+    }
+
+
+
+    @Override
+    public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView,
+                            @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY,
+                            int actionState, boolean isCurrentlyActive) {
+
+        View itemView = viewHolder.itemView;
+        int bound1 = 110;
+        int bound2 = itemView.getWidth() - 110;
+        int backgroundCornerOffset = 1;
+
+        int iconMargin = (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
+        int iconTop = itemView.getTop() + (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
+        int iconBottom = iconTop + icon.getIntrinsicHeight();
+
+        Log.d("margin", ":" + iconMargin);
+        Log.d("Top", ":" + iconTop);
+        Log.d("Bottom", ":" + iconBottom);
+        Log.d("intrinsicHeight", ":" + icon.getIntrinsicHeight());
+
+        if (dX > 0) { // Swiping to the right
+
+            //these were swapped in the example
+            int iconLeft = itemView.getLeft() + iconMargin;
+            int iconRight = itemView.getLeft() + iconMargin + icon.getIntrinsicWidth();
+
+            //icon.setBounds(0,iconTop,48,iconBottom);
+            icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
+            background.setBounds(itemView.getLeft(), itemView.getTop(),
+                    itemView.getLeft() + ((int) dX) + backgroundCornerOffset,
+                    itemView.getBottom());
+
+
+            background.draw(c);
+            if (itemView.getLeft() + ((int) dX) + backgroundCornerOffset > bound1){
+                icon.draw(c);
+            }
+
+        } else if (dX < 0) { // Swiping to the left
+
+            int iconLeft = itemView.getRight() - iconMargin - icon.getIntrinsicWidth();
+            int iconRight = itemView.getRight() - iconMargin;
+            icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
+            background.setBounds(itemView.getRight() + ((int) dX) - backgroundCornerOffset,
+                    itemView.getTop(), itemView.getRight(), itemView.getBottom());
+
+            background.draw(c);
+            if (itemView.getRight() + ((int) dX) - backgroundCornerOffset < bound2){
+                icon.draw(c);
+            }
+
+            Log.d("itemviewWidth", ":" + itemView.getWidth());
+        } else { // view is unSwiped (dX = 0 ??)
+            background.setBounds(0, 0, 0, 0);
+            icon.setBounds(0, 0, 0, 0);
+        }
+
+        //background.draw(c);
+        //icon.draw(c);
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+    }
+}
