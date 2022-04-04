96bb2bc43abe55a988ff0937971bd6b488105da3
Mutigo <kai.aslan1996@gmail.com>
2022-04-04 22:25:57 +0200

    Added warnings before leaving sections
    Because:
    - user gets feedback when leaving a fragment
    - user gets information about lost data
    
    User gets informed that if the user got unsaved data, he should save it or finish the process before leaving

--- app/src/main/java/com/example/vpmanager/views/mainActivity.java
+++ app/src/main/java/com/example/vpmanager/views/mainActivity.java
@@ -9,6 +9,8 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -37,6 +39,8 @@ public class mainActivity extends AppCompatActivity implements DrawerController 
     private NavigationView navigationViewMain;
     private NavHostFragment navHostFragment;
     private FirebaseAuth firebaseAuth;
+    private boolean userStay = true;
+
 
     private MaterialToolbar topAppBarMain;
     private AppBarConfiguration appBarConfiguration;
@@ -92,8 +96,7 @@ public class mainActivity extends AppCompatActivity implements DrawerController 
         //navController = Navigation.findNavController(this, R.id.nav_host_fragment_main);
 
         //Top level destinations are configured here. createStudyActivity and studyFragment should not be included!
-        appBarConfiguration = new AppBarConfiguration.Builder(R.id.homeFragment, R.id.findStudyFragment,
-                R.id.createStudyFragment, R.id.ownStudyFragment).setDrawerLayout(drawerLayoutMain).build();
+        appBarConfiguration = new AppBarConfiguration.Builder(R.id.homeFragment, R.id.findStudyFragment, R.id.ownStudyFragment).setDrawerLayout(drawerLayoutMain).build();
 
         //handle Navigation item clicks
         //this works with no further action, if the menu and destination id’s match.
@@ -134,6 +137,21 @@ public class mainActivity extends AppCompatActivity implements DrawerController 
         });
 
         /*
+        topAppBarMain.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(currentFragment.equals("createBase")){
+                    System.out.println("HUEHEUHEUHEUEHEUHEUHE");
+                }
+                else {
+                    drawerLayoutMain.open();
+                }
+            }
+        });
+        */
+
+
+        /*
         navigationViewMain.getMenu().getItem(1).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
@@ -169,10 +187,57 @@ public class mainActivity extends AppCompatActivity implements DrawerController 
     //allows NavigationUI to support proper up navigation (in the Action Bar)
     @Override
     public boolean onSupportNavigateUp() {
-      
+        if (userStay) {
+            if (currentFragment.equals("createBase") || currentFragment.equals("createStepOne") || currentFragment.equals("createStepTwo")
+                    || currentFragment.equals("createStepThree") || currentFragment.equals("createStepFourRemote") || currentFragment.equals("createStepFourPresence")
+                    || currentFragment.equals("createStepFive") || currentFragment.equals("createFinalStep") || currentFragment.equals("createFinalStepTwo") ||
+                    currentFragment.equals("createFinalStepThree") || currentFragment.equals("editFragment")) {
+                Dialog dialog = new Dialog(this, R.style.DialogStyle);
+                dialog.setContentView(R.layout.duplicate_dialog);
+                dialog.getWindow().setBackgroundDrawableResource(R.drawable.bg_window);
+                ImageView btnClose = dialog.findViewById(R.id.btn_close);
+                btnClose.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+                TextView title = dialog.findViewById(R.id.info_title);
+                TextView desc = dialog.findViewById(R.id.duplicate_desc);
+                CheckBox box = dialog.findViewById(R.id.doNotShowAgainCheckBox);
+                box.setVisibility(View.GONE);
+                Button stay = dialog.findViewById(R.id.abort);
+                Button leave = dialog.findViewById(R.id.addAnyways);
+                ImageView icon = dialog.findViewById(R.id.img_icon);
+                icon.setImageDrawable(getDrawable(R.drawable.ic_baseline_warning_24));
+
+                title.setText("Achtung!");
+                desc.setText("Wenn Sie diesen Bereich verlassen gehen Ihre Änderungen verloren! Wollen Sie trotzdem verlassen?");
+                stay.setText("Abbrechen");
+                leave.setText("Verlassen");
+                dialog.show();
+
+                stay.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+                leave.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        userStay = false;
+                        dialog.dismiss();
+                        onSupportNavigateUp();
+                    }
+                });
+                return false;
+            }
+        }
+        userStay = true;
         return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
     }
-
 
 
     //Parameter: context
@@ -214,15 +279,15 @@ public class mainActivity extends AppCompatActivity implements DrawerController 
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.app_bar_menu, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item){
-        if(item.getItemId() == R.id.pageInfoIcon){
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.pageInfoIcon) {
             showInfoText();
         }
         return super.onOptionsItemSelected(item);
@@ -246,98 +311,148 @@ public class mainActivity extends AppCompatActivity implements DrawerController 
         TextView title = dialog.findViewById(R.id.info_title);
         TextView desc = dialog.findViewById(R.id.info_desc);
 
-       switch (currentFragment){
-           case "home":
-               title.setText(getString(R.string.infoPaMainTitle));
-               desc.setText(getString(R.string.infoPaMain));
-               break;
-           case "findStudy":
-               title.setText(getString(R.string.infoFindStudyTitle));
-               desc.setText(getString(R.string.infoFindStudies));
-               break;
-           case "login":
-               title.setText(getString(R.string.infoLoginTitle));
-               desc.setText(getString(R.string.infoLogin));
-               break;
-           case "register":
-               title.setText(getString(R.string.register_info_title));
-               desc.setText(getString(R.string.register_info_desc));
-               break;
-           case "upcomingAppointments":
-               title.setText(getString(R.string.infoPAUpcomingTitle));
-               desc.setText(getString(R.string.infoPaUpcoming));
-               break;
-           case "editFragment":
-               title.setText(getString(R.string.infoEditFragmentTitle));
-               desc.setText(getString(R.string.infoEditStudy));
-               break;
-           case "studyFragment":
-               title.setText(getString(R.string.infoStudyDetailsTitle));
-               desc.setText(getString(R.string.infoStudyDetails));
-               break;
-           case "studyCreatorFragment":
-               title.setText(getString(R.string.infoCreatorStudyDetailsTitle));
-               desc.setText(getString(R.string.infoStudyCreatorDetails));
-               break;
-           case "ownStudyFragment":
-               title.setText(getString(R.string.infoOwnStudiesTitle));
-               desc.setText(getString(R.string.infoOwnStudies));
-               break;
-           case "createBase":
-               title.setText(getString(R.string.infoCreateStartTitle));
-               desc.setText(getString(R.string.infoCreateStart));
-               break;
-           case "createStepOne":
-               title.setText(getString(R.string.infoCreateBaseTitle));
-               desc.setText(getString(R.string.infoCreateBase));
-               break;
-           case "createStepTwo":
-               title.setText(getString(R.string.infoCreateContactTitle));
-               desc.setText(getString(R.string.infoCreateContact));
-               break;
-           case "createStepThree":
-               title.setText(getString(R.string.infoCreateDescTitle));
-               desc.setText(getString(R.string.infoCreateDescription));
-               break;
-           case "createStepFourRemote":
-               title.setText(getString(R.string.infoCreatePlatformTitle));
-               desc.setText(getString(R.string.infoCreatePlatform));
-               break;
-           case "createStepFourPresence":
-               title.setText(getString(R.string.infoCreateLocationTitle));
-               desc.setText(getString(R.string.infoCreateLocation));
-               break;
-           case "createStepFive":
-               title.setText(getString(R.string.infoCreateDatesTitle));
-               desc.setText(getString(R.string.infoCreateDates));
-               break;
-           case "createFinalStep":
-               title.setText(getString(R.string.infoCreateConfirmDetailsTitle));
-               desc.setText(getString(R.string.infoCreateConfirmDetails));
-               break;
-           case "createFinalStepTwo":
-               title.setText(getString(R.string.infoCreateConfirmDetailsTitle));
-               desc.setText(getString(R.string.infoCreateConfirmDetails));
-               break;
-           case "createFinalStepThree":
-               title.setText(getString(R.string.infoCreateConfirmDatesTitle));
-               desc.setText(getString(R.string.infoCreateConfirmDates));
-               break;
-           default:
-               break;
-       }
+        switch (currentFragment) {
+            case "home":
+                title.setText(getString(R.string.infoPaMainTitle));
+                desc.setText(getString(R.string.infoPaMain));
+                break;
+            case "findStudy":
+                title.setText(getString(R.string.infoFindStudyTitle));
+                desc.setText(getString(R.string.infoFindStudies));
+                break;
+            case "login":
+                title.setText(getString(R.string.infoLoginTitle));
+                desc.setText(getString(R.string.infoLogin));
+                break;
+            case "register":
+                title.setText(getString(R.string.register_info_title));
+                desc.setText(getString(R.string.register_info_desc));
+                break;
+            case "upcomingAppointments":
+                title.setText(getString(R.string.infoPAUpcomingTitle));
+                desc.setText(getString(R.string.infoPaUpcoming));
+                break;
+            case "editFragment":
+                title.setText(getString(R.string.infoEditFragmentTitle));
+                desc.setText(getString(R.string.infoEditStudy));
+                break;
+            case "studyFragment":
+                title.setText(getString(R.string.infoStudyDetailsTitle));
+                desc.setText(getString(R.string.infoStudyDetails));
+                break;
+            case "studyCreatorFragment":
+                title.setText(getString(R.string.infoCreatorStudyDetailsTitle));
+                desc.setText(getString(R.string.infoStudyCreatorDetails));
+                break;
+            case "ownStudyFragment":
+                title.setText(getString(R.string.infoOwnStudiesTitle));
+                desc.setText(getString(R.string.infoOwnStudies));
+                break;
+            case "createBase":
+                title.setText(getString(R.string.infoCreateStartTitle));
+                desc.setText(getString(R.string.infoCreateStart));
+                break;
+            case "createStepOne":
+                title.setText(getString(R.string.infoCreateBaseTitle));
+                desc.setText(getString(R.string.infoCreateBase));
+                break;
+            case "createStepTwo":
+                title.setText(getString(R.string.infoCreateContactTitle));
+                desc.setText(getString(R.string.infoCreateContact));
+                break;
+            case "createStepThree":
+                title.setText(getString(R.string.infoCreateDescTitle));
+                desc.setText(getString(R.string.infoCreateDescription));
+                break;
+            case "createStepFourRemote":
+                title.setText(getString(R.string.infoCreatePlatformTitle));
+                desc.setText(getString(R.string.infoCreatePlatform));
+                break;
+            case "createStepFourPresence":
+                title.setText(getString(R.string.infoCreateLocationTitle));
+                desc.setText(getString(R.string.infoCreateLocation));
+                break;
+            case "createStepFive":
+                title.setText(getString(R.string.infoCreateDatesTitle));
+                desc.setText(getString(R.string.infoCreateDates));
+                break;
+            case "createFinalStep":
+                title.setText(getString(R.string.infoCreateConfirmDetailsTitle));
+                desc.setText(getString(R.string.infoCreateConfirmDetails));
+                break;
+            case "createFinalStepTwo":
+                title.setText(getString(R.string.infoCreateConfirmDetailsTitle));
+                desc.setText(getString(R.string.infoCreateConfirmDetails));
+                break;
+            case "createFinalStepThree":
+                title.setText(getString(R.string.infoCreateConfirmDatesTitle));
+                desc.setText(getString(R.string.infoCreateConfirmDates));
+                break;
+            default:
+                break;
+        }
         dialog.show();
 
     }
 
     @Override
     public void onBackPressed() {
-        if (drawerLayoutMain.isOpen())
-        {
+        if (drawerLayoutMain.isOpen()) {
             drawerLayoutMain.close();
-        }
-        else {
+        } else {
+            if (currentFragment.equals("createBase") || currentFragment.equals("createStepOne") || currentFragment.equals("createStepTwo")
+                    || currentFragment.equals("createStepThree") || currentFragment.equals("createStepFourRemote") || currentFragment.equals("createStepFourPresence")
+                    || currentFragment.equals("createStepFive") || currentFragment.equals("createFinalStep") || currentFragment.equals("createFinalStepTwo") ||
+                    currentFragment.equals("createFinalStepThree") || currentFragment.equals("editFragment")) {
+                if (userStay) {
+                    Dialog dialog = new Dialog(this, R.style.DialogStyle);
+                    dialog.setContentView(R.layout.duplicate_dialog);
+                    dialog.getWindow().setBackgroundDrawableResource(R.drawable.bg_window);
+                    ImageView btnClose = dialog.findViewById(R.id.btn_close);
+                    btnClose.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            dialog.dismiss();
+                        }
+                    });
+                    TextView title = dialog.findViewById(R.id.info_title);
+                    TextView desc = dialog.findViewById(R.id.duplicate_desc);
+                    CheckBox box = dialog.findViewById(R.id.doNotShowAgainCheckBox);
+                    box.setVisibility(View.GONE);
+                    Button stay = dialog.findViewById(R.id.abort);
+                    Button leave = dialog.findViewById(R.id.addAnyways);
+                    ImageView icon = dialog.findViewById(R.id.img_icon);
+                    icon.setImageDrawable(getDrawable(R.drawable.ic_baseline_warning_24));
+
+                    title.setText("Achtung!");
+                    desc.setText("Wenn Sie diesen Bereich verlassen gehen Ihre Änderungen verloren! Wollen Sie trotzdem verlassen?");
+                    stay.setText("Abbrechen");
+                    leave.setText("Verlassen");
+                    dialog.show();
+
+                    stay.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            dialog.dismiss();
+                        }
+                    });
+
+                    leave.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            userStay = false;
+                            dialog.dismiss();
+                            onBackPressed();
+                        }
+                    });
+
+                } else {
+                    userStay = true;
+                    super.onBackPressed();
+                }
+            } else {
                 super.onBackPressed();
+            }
         }
     }
 }
