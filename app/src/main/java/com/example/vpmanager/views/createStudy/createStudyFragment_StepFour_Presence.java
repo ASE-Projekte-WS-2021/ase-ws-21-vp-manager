31e371670de3ae03069136237d72a251c098d0a7
Mutigo <kai.aslan1996@gmail.com>
2022-04-06 16:03:21 +0200

    Removed unusued imports
    Because:
    -improve code readability

--- app/src/main/java/com/example/vpmanager/views/createStudy/createStudyFragment_StepFour_Presence.java
+++ app/src/main/java/com/example/vpmanager/views/createStudy/createStudyFragment_StepFour_Presence.java
@@ -7,7 +7,6 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 
@@ -101,7 +100,6 @@ public class createStudyFragment_StepFour_Presence extends Fragment {
                 }
             }
         });
-
     }
 
     //Parameter:
